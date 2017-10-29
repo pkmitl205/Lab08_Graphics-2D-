b7ba64e11ce19c06fa5bd9efbe6303883dd957de
pkmitl205 <58030205@kmitl.ac.th>
2017-10-29 22:29:42 +0700

    การโหลดภาพชนิดต่างๆ มาแสดงบนฟอร์ม

--- Lab08/Lab08/Form1.Designer.cs
+++ Lab08/Lab08/Form1.Designer.cs
@@ -28,43 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 34);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(12, 52);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 37);
-            this.panel2.TabIndex = 1;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(13, 96);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(199, 153);
-            this.panel3.TabIndex = 2;
-            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -73,10 +43,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- Lab08/Lab08/Form1.cs
+++ Lab08/Lab08/Form1.cs
@@ -21,36 +21,10 @@ namespace Lab08
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+            Bitmap bmp = new Bitmap("D:\\Capture.PNG");
+            this.SetClientSizeCore(bmp.Width + 20, bmp.Height + 20);
+            e.Graphics.DrawImage(bmp, 10, 10);
             g.Dispose();
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel1.ClientRectangle,
-            Color.Yellow, Color.Navy, 0f, true);
-
-            e.Graphics.FillRectangle(pnlGdt, panel1.ClientRectangle);
-            pnlGdt.Dispose();
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel2.ClientRectangle,
-            Color.Yellow, Color.Navy, 90f, true);
-
-            e.Graphics.FillRectangle(pnlGdt, panel2.ClientRectangle);
-            pnlGdt.Dispose();
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-            GraphicsPath path = new GraphicsPath();
-            path.AddEllipse(panel3.ClientRectangle);
-            PathGradientBrush br = new PathGradientBrush(path);
-            br.CenterPoint = new PointF(panel3.ClientRectangle.Width / 2, panel3.ClientRectangle.Height / 2);
-            br.CenterColor = Color.Navy;
-            br.SurroundColors = new Color[] { Color.Yellow };
-            e.Graphics.FillPath(br, path);
         }
     }
 }
